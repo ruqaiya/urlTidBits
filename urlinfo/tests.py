d008dc43a9edef6c438952db4de03197671d03f7
Ruqaiya Saad Khan <ruqaiyakhn90@gmail.com>
2017-11-06 20:42:20 +0500

    creating unit test branch

--- urlinfo/tests.py
+++ urlinfo/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from urlinfo.views import home
+
+class HomeTestCase(TestCase):
+    def setUp(self):
+		pass        
+
+    def test_final_home_view(self):
+        """
+        Given a URL; test all required info is being generated.
+        """
+        pass
